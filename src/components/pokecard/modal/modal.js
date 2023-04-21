e24d9d7225367217623027ee8c5f4cff20aa3cb0
knopgm <knop.gm@gmail.com>
2023-04-21 23:09:23 +0200

    Create a Modal component for each pokecard

---
+++ src/components/pokecard/modal/modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./modal.scss";
+
+export function Modal() {
+  return (
+    <div>
+      <h1>Modal Title</h1>
+      <p>Modal content</p>
+    </div>
+  );
+}
